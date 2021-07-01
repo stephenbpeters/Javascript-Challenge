d6f89887326d8a4ca5285b4190648dbcf4efd1a2
stephenbpeters <stephen.peters@gmail.com>
2021-07-01 10:59:27 -0700

    level 1 complete

---
+++ UFO-level-1/static/ufo-level-1.js
@@ -0,0 +1,108 @@
+// Assign the data from `data.js` to a descriptive variable
+
+var ufos = data;
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  //var inputElement = d3.select("#ufo-state-input");
+
+  // Get the value property of the input element
+  //var inputValue = inputElement.property("value");
+
+  //console.log('inputValue = ', inputValue);
+
+  // let's get the shape input field text
+   // Select the input element and get the raw HTML node
+   //var inputElement2 = d3.select("#ufo-shape-input");
+
+   // Get the value property of the input element
+  //  var inputValue2 = inputElement2.property("value");
+ 
+  //  console.log('inputValue2 = ', inputValue2);
+
+  // let's get the date input field text
+   // Select the input element and get the raw HTML node
+   var inputElement3 = d3.select("#ufo-date-input");
+
+   // Get the value property of the input element
+   var inputValue3 = inputElement3.property("value");
+ 
+   console.log('inputValue3 = ', inputValue3);
+
+  // let's get the city input field text
+   // Select the input element and get the raw HTML node
+  //  var inputElement4 = d3.select("#ufo-city-input");
+
+  //  // Get the value property of the input element
+  //  var inputValue4 = inputElement4.property("value");
+ 
+  //  console.log('inputValue4 = ', inputValue4);
+
+  // let's get the country input field text
+   // Select the input element and get the raw HTML node
+  //  var inputElement5 = d3.select("#ufo-country-input");
+
+  //  // Get the value property of the input element
+  //  var inputValue5 = inputElement5.property("value");
+ 
+  //  console.log('inputValue5 = ', inputValue5);
+
+  // we only need to filter by date/time for level 1, so turn stuff off here
+  //var filteredOtherData = ufos.filter(ufo => ufo.state === inputValue);
+
+  // run a second filter
+  //var filteredOtherData2 = filteredOtherData.filter(ufo => ufo.shape === inputValue2);
+
+  // run a third filter
+  // copy and edit this -->
+  //var filteredOtherData3 = filteredOtherData2.filter(ufo => ufo.datetime === inputValue3);
+  var filteredOtherData3 = data.filter(ufo => ufo.datetime === inputValue3);
+
+  // run a fourth filter
+  //var filteredOtherData4 = filteredOtherData3.filter(ufo => ufo.city === inputValue4);
+
+  // run a fifth filter
+  //var filteredOtherData5 = filteredOtherData4.filter(ufo => ufo.country === inputValue5);  
+
+
+// print out our data
+  // Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// let's see if we can clear the table with this command:
+tbody.html("");
+
+// here's our working one-filter print out stuff:
+  // filteredOtherData.forEach(item => {
+  //   var row = tbody.append("tr");
+  //   Object.entries(item).forEach(([key, value]) => {
+  //     var cell = row.append("td");
+  //     cell.text(value);
+  //   });
+  // });
+
+//   
+  filteredOtherData3.forEach(item => {
+    var row = tbody.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+};
